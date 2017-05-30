55c398e6fce386ccad77ec23e19d14be6d77b75f
danremel <remel.dan@gmail.com>
2017-05-30 11:10:28 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 console.log("Up and running!");
 
-var cardOne = "King";
-var cardTwo = "King";
-var cardThree = "Queen";
-var cardFour = "Queen";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!"); }
+	else {
+	alert("Sorry, try again.");
+}};
